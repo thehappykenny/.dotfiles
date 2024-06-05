25795be6de0981634ce9d7ee7a12b7c8aa085900
wp <859994178@qq.com>
2024-06-05 23:41:26 +0800

    proxy_git

---
+++ tag-bin/bin/tools/proxy_git.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+function proxy_on() {
+	export http_proxy="http://127.0.0.1:38457"
+	export https_proxy=$http_proxy
+	echo -e "终端代理开启。"
+}
+
+function proxy_off() {
+	unset http_proxy https_proxy
+	echo -e "终端代理关闭。"
+}
